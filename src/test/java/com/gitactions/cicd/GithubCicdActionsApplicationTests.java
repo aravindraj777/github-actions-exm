4181d65611291fc75a5b70a1477c78a75ed609cd
Aravind <aravindmunna240@gmail.com>
2024-01-15 13:04:58 +0530

    code

---
+++ src/test/java/com/gitactions/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitactions.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
